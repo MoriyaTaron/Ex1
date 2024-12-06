f478ac397a8128f19154e26c228fb342755d5f26
moria <moria543210@gmail.com>
2024-12-06 15:20:02 +0200

    אני חושבת שהלכ עובד עכשיו.. נשאר צלי לעשות טסטים ולבדוק האם הפונקציות באמת עובדות כמו שזה נראה. רשמתי את המיין פשוט אחד לאחד כמו בפיתרון שלהם אז כדאי שאני אבדוק אם זה מה שבאמת היה צריך אם לא אז אני אשנה. וגם אני חייבת לבדוק למה נועדה הפונקציה של הequals!! כי לא השתמשתי בה בכלל בmain ואני צריכה להבין האם פספסתי עוד משהו שצריך לכתוב בקשר לזה כי זה מוזר שלא משתמשים בפונקציה הזאת. אני צריה לבדוק מה זה בדיוק ה readme

--- src/Ex1Main.java
+++ src/Ex1Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -7,11 +8,6 @@ import java.util.Scanner;
  */
 public class Ex1Main {
     public static void main(String[] args) {
-        if (Ex1.isNumber("000b2"))
-        System.out.println("grate");
-        else
-        System.out.println("no no no");
-        System.out.println();
         Scanner sc = new Scanner(System.in);
         String num1 = "", num2="", quit = "quit";
         while (!num1.equals(quit) && !num2.equals(quit)) {
@@ -20,7 +16,35 @@ public class Ex1Main {
             System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
             num1 = sc.next();
             if (!num1.equals("quit")) {
-                // add your code here
+                System.out.println("num1="+num1+ " is number: "+Ex1.isNumber(num1)+" value: "+Ex1.number2Int(num1));
+                if (!Ex1.isNumber(num1)){
+                    System.out.println("the number num1 is in the wrong format you can try again... ");
+                }else {
+                    System.out.println("Enter a string as number#2 (or \"quit\" to end the program)");
+                    num2= sc.next();
+                    if (!num2.equals("quit")){
+                        System.out.println("num2="+num2+" is number: "+Ex1.isNumber(num2)+" value: "+Ex1.number2Int(num2));
+                        if (!Ex1.isNumber(num2)){
+                            System.out.println("the number num2 is in the wrong format you can try again... ");
+                        }
+                        else {
+                            System.out.println("Enter a base for output: number between 2 to 16");
+                            int base = sc.nextInt();
+                            if (base<2||base>16){
+                                System.out.println("That's not what i asked for you can try it all again....");
+                            }else {
+                                int plus = Ex1.number2Int(num1)+Ex1.number2Int(num2);
+                                int mult= Ex1.number2Int(num1)*Ex1.number2Int(num2);
+                                String[] solutions= {num1,num2,Ex1.int2Number(plus,base),Ex1.int2Number(mult,base)};
+                                System.out.println(num1+" + "+num2+" = "+Ex1.int2Number(plus,base));
+                                System.out.println(num1+" * "+num2+" = "+Ex1.int2Number(mult,base));
+                                System.out.println("Max number over"+ Arrays.toString(solutions)+"is: "+solutions[Ex1.maxIndex(solutions)]);
+
+                            }
+                        }
+                    }
+
+                }
 
                 /////////////////////
             }
